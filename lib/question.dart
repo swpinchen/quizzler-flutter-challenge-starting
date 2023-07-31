2d8ac3033b1776c15734866f1750b1ca1bd74863
swpinchen <swpinchen@outlook.com>
2023-07-31 17:23:18 +0900

    Updated constructor to Dart shorthand

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,11 @@ class Question {
   String questionText = '';
   bool questionAnswer = false;
 
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  // Question(String q, bool a) {
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+
+  // Dart shorthand for initializing formal parameters
+  Question(this.questionText, this.questionAnswer);
 }
